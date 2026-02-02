0a68d72585ea92abc7bf29939394c49c98d3d9b1
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-02-01 18:29:30 -0700

    deleted unused images and commented out pricing section for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./Hero";
 import About from "./sections/About";
 import Features from "./sections/Features";
-import Pricing from "./sections/Pricing";
+// import Pricing from "./sections/Pricing";
 import Services from "./sections/Services";
 import Testimonials from "./sections/Testimonials";
 
@@ -16,7 +16,7 @@ export default function Home() {
 			<About />
 			<Services />
 			<Features />
-			<Pricing />
+			{/* <Pricing /> */}
 			<Testimonials />
 		</>
 	);
